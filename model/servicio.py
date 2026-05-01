46220d4216415fa7fdca07aa719066855111c0f4
karina <kpdiazarunad@gmail.com>
2026-04-30 22:26:23 -0500

    Create Servicio abstract class with methods

---
+++ model/servicio.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class Servicio(ABC):
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    @abstractmethod
+    def calcular_costo(self):
+        pass
+
+    @abstractmethod
+    def descripcion(self):
+        pass
