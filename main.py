a3bb89ea370ff836efb724e6f34f765a78561ff5
karina <kpdiazarunad@gmail.com>
2026-05-08 22:05:56 -0500

    Corrección e integración de main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from modelo.cliente import Cliente
-from modelo.servicio import Servicio
+from modelo.servicio import ReservaSala
 from modelo.reserva import Reserva
 
 try:
-    cliente1 = Cliente("Karina Diaz", "karina@email.com")
-    servicio1 = Servicio("Reserva de sala", 50)
+    cliente1 = Cliente("Karina Diaz", 22)
+    servicio1 = ReservaSala("Sala principal", 50)
 
     reserva1 = Reserva(cliente1, servicio1)
     reserva1.confirmar()
 
     print("Reserva creada correctamente")
     print("Estado:", reserva1.estado)
+    print("Costo:", servicio1.calcular_costo(2))
 
 except Exception as e:
     print("Error:", e)
