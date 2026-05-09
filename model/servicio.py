7dcde85d83302c0f3c73b153da1d6643b8ae49fe
karina <kpdiazarunad@gmail.com>
2026-05-08 21:58:31 -0500

    Se integra clase concreta de servicio y archivo main

--- model/servicio.py
+++ model/servicio.py
@@ -11,3 +11,13 @@ class Servicio(ABC):
     @abstractmethod
     def descripcion(self):
         pass
+class ReservaSala(Servicio):
+    def __init__(self, nombre, precio_hora):
+        super().__init__(nombre)
+        self.precio_hora = precio_hora
+
+    def calcular_costo(self, horas):
+        return self.precio_hora * horas
+
+    def descripcion(self):
+        return f"Servicio de reserva de sala: {self.nombre}"
