5b0521805b0d0cb1c8148d6b5e8f574c89bac532
karina <kpdiazarunad@gmail.com>
2026-05-09 16:50:30 -0500

    Actualizar clase Servicio abstracta
    Removed constructor and abstract methods from Servicio class.

--- model/servicio.py
+++ model/servicio.py
@@ -1,23 +1,7 @@
 from abc import ABC, abstractmethod
 
 class Servicio(ABC):
-    def __init__(self, nombre):
-        self.nombre = nombre
 
     @abstractmethod
     def calcular_costo(self):
         pass
-
-    @abstractmethod
-    def descripcion(self):
-        pass
-class ReservaSala(Servicio):
-    def __init__(self, nombre, precio_hora):
-        super().__init__(nombre)
-        self.precio_hora = precio_hora
-
-    def calcular_costo(self, horas):
-        return self.precio_hora * horas
-
-    def descripcion(self):
-        return f"Servicio de reserva de sala: {self.nombre}"
