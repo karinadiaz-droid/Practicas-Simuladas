cc3db654efa8d0d5d0c819ec3bb5a97c931005b8
rafael246 <138317401+rafael246@users.noreply.github.com>
2026-05-11 22:08:36 -0500

    Update cliente.py

--- model/cliente.py
+++ model/cliente.py
@@ -1,9 +1,36 @@
+import re
+from datetime import datetime
+from .excepciones import ClienteInvalidoError
+
 class Cliente:
+    _id_counter = 1
 
-    def __init__(self, nombre, edad):
+    def __init__(self, nombre, email, telefono):
+        self.id = Cliente._id_counter
+        Cliente._id_counter += 1
+        self._nombre = None
+        self._email = None
+        self._telefono = None
+        self.activo = True
+        self.fecha_registro = datetime.now()
+        #hacemos validaciones a traves de setters
+        self.nombre = nombre
+        self.email = email
+        self.telefono = telefono
 
-        if not nombre:
-            raise ValueError("Nombre inválido")
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, valor):
+         if not valor or len(valor.strip()) < 3:
+            raise ClienteInvalidoError(f"Nombre inválido: '{valor}'. mínimo 3 letras")
+        self._nombre = valor.strip().title()
+
+    @property
+
+        
 
         if edad <= 0:
             raise ValueError("Edad inválida")
