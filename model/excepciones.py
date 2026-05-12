c6a4be285807f93432d1dbf4234caa0dbd0c74ba
rafael246 <138317401+rafael246@users.noreply.github.com>
2026-05-11 23:08:07 -0500

    Update excepciones.py

--- model/excepciones.py
+++ model/excepciones.py
@@ -1,23 +1,18 @@
-class SistemaReservasError(Exception):
+# Excepciones personalizadas del sistema
+class SistemaError(Exception): #con esto se define una nueva clase llamada SistemaError
+    #(Exception) indica que esta clase hereda de la clase Exception (que es la clase base para todas las excepciones en Python)
     """Excepción base del sistema"""
     pass
 
-class ClienteInvalidoError(SistemaReservasError):
-    """Error relacionado con datos inválidos del cliente"""
-    pass
+class ClienteInvalidoError(SistemaError): #ClienteInvalidoError es un tipo específico de SistemaError.
+    """Error en datos del cliente"""
+    pass #en Python, los bloques (como el cuerpo de una clase o función) no pueden estar vacíos. El pass sirve como un marcador de posición para indicar "no hay contenido adicional"
 
-class ServicioNoDisponibleError(SistemaReservasError):
+class ServicioNoDisponibleError(SistemaError): #se lanza cuando se intenta reservar un servicio que tiene disponible = False 
+                                               # (desactivado por el administrador), o cuando el servicio no existe. Así se distingue de otros errores de reserva.
     """Error cuando un servicio no está disponible"""
     pass
 
-class ReservaInvalidaError(SistemaReservasError):
-    """Error en la creación o modificación de reservas"""
-    pass
-
-class ParametroFaltanteError(SistemaReservasError):
-    """Error cuando faltan parámetros obligatorios"""
-    pass
-
-class CalculoInconsistenteError(SistemaReservasError):
-    """Error en cálculos de costos"""
+class ReservaInvalidaError(SistemaError): #se lanza cuando los datos de la reserva no cumplen las reglas de negocio como]: cliente inactivo, duración no positiva, o cualquier otra inconsistencia
+    """Error en la creación o gestión de reservas"""
     pass
